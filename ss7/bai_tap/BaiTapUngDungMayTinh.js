cd92d1cef0f6b1cd66f8e13228fb8b3e3f298a90
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-01-05 17:21:07 +0700

    nop bai tap

---
+++ ss7/bai_tap/BaiTapUngDungMayTinh.js
@@ -0,0 +1,25 @@
+function toanCong() {
+let AD= document.getElementById("ADD").value;
+let I1=parseInt( document.getElementById("I1").value);
+let I2=parseInt(document.getElementById("I2").value);
+alert(I1+I2)
+}
+function toanTru() {
+    let sub= document.getElementById("SUB").value;
+    let I1= document.getElementById("I1").value;
+    let I2= document.getElementById("I2").value;
+    alert(I1-I2)
+}
+
+function toanNhan() {
+    let mul= document.getElementById("MUL").value;
+    let I1= document.getElementById("I1").value;
+    let I2= document.getElementById("I2").value;
+    alert(I1*I2)
+}
+function toanChia() {
+    let div= document.getElementById("DEV").value;
+    let I1= document.getElementById("I1").value;
+    let I2= document.getElementById("I2").value;
+    alert(I1/I2)
+}
